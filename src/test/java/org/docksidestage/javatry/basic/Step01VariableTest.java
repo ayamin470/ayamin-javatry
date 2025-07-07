b493fb67f867c3f99e060986df4655e5b08d8d93
jflute <dbflute@gmail.com>
2025-07-07 13:30:27 +0900

    todoのdoneについて、slackへの誘導のtodoを追加

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -95,8 +95,9 @@ public class Step01VariableTest extends PlainTestCase
     // ↓の「レビューのやり取り」の欄を参考にお願いします。
     // https://dbflute.seasar.org/ja/tutorial/handson/review/jflutereview.html#review
 
-    // TODO done jflute さん by ayamin
+    // done jflute さん by ayamin
     // TODOにはこのように返事をすると言うこと。理解しました。次回から修正いたします
+    // TODO ayamin 改めて、Slackでtodoのdoneについて補足していますので、Slackご覧ください by jflute (2025/07/07)
 
     // ===================================================================================
     //                                                                   Instance Variable
