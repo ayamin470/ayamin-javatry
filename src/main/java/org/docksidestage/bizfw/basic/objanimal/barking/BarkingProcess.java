e153c463fc2d80d33d80a0975cb1bf2f9531c390
“ayamin470” <ayami.hatano@bizreach.co.jp>
2025-07-18 14:58:42 +0900

    TDOOの確認

---
+++ src/main/java/org/docksidestage/bizfw/basic/objanimal/barking/BarkingProcess.java
@@ -0,0 +1,56 @@
+package org.docksidestage.bizfw.basic.objanimal.barking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The process for animal barking.
+ * (動物の鳴き声のプロセス)
+ * @author jflute (or your name)
+ */
+public class BarkingProcess {
+
+    private static final Logger logger = LoggerFactory.getLogger(BarkingProcess.class);
+
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
+    public BarkingProcess() {
+    }
+
+    // ===================================================================================
+    //                                                                               Bark
+    //                                                                              ======
+    /**
+     * Perform the complete barking process.
+     * (鳴き声の完全なプロセスを実行する)
+     * @param barkWord The word the animal barks. (動物が鳴く言葉)
+     * @param hitPointCallback Callback to decrease hit point. (ヒットポイントを減らすためのコールバック)
+     * @return The sound created by barking. (鳴き声によって生成された音)
+     */
+    public BarkedSound bark(String barkWord, Runnable hitPointCallback) {
+        // Animalクラスから移動してきたロジック
+        breatheIn(hitPointCallback);
+        prepareAbdominalMuscle(hitPointCallback);
+        BarkedSound barkedSound = doBark(barkWord, hitPointCallback);
+        return barkedSound;
+    }
+
+    // Animalクラスから移動してきたメソッド
+    protected void breatheIn(Runnable hitPointCallback) {
+        logger.debug("...Breathing in for barking");
+        hitPointCallback.run(); // ヒットポイント減少処理をコールバックで呼び出す
+    }
+
+    // Animalクラスから移動してきたメソッド
+    protected void prepareAbdominalMuscle(Runnable hitPointCallback) {
+        logger.debug("...Using my abdominal muscle for barking");
+        hitPointCallback.run(); // ヒットポイント減少処理をコールバックで呼び出す
+    }
+
+    // Animalクラスから移動してきたメソッド
+    protected BarkedSound doBark(String barkWord, Runnable hitPointCallback) {
+        hitPointCallback.run(); // ヒットポイント減少処理をコールバックで呼び出す
+        return new BarkedSound(barkWord);
+    }
+}
