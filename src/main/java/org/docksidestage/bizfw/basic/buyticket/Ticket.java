f369d1b2d8b7562f441761a2444ae8dba2bf25c9
ayamin470 <ayami.hatano@bizreach.co.jp>
2025-08-05 14:46:53 +0900

    Step05のTODOを少し解きました(1時間でできたところまで)

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/Ticket.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/Ticket.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,11 @@
  */
 package org.docksidestage.bizfw.basic.buyticket;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
+ * 遊園地のチケットを表すクラス
+ * チケットの情報(有効日数、価格など)と、入園処理を定義
  * @author jflute
  * @author ayamin
  */
@@ -25,16 +28,23 @@ public class Ticket {
     // ===================================================================================
     //                                                                           Attribute
     //                                                                           =========
-    // done ayamin [いいね] インスタンス変数の定義順序、Constructorの引数や設定と合っててとても綺麗で直感的で良いです！ by jflute (2025/07/07)
-    private final int displayPrice; // written on ticket, park guest can watch this
+    private final int displayPrice;
     private final int validDays;
-    private final boolean nightOnly;// チケットが有効な日数 (例: 1日パスポートなら1, 2日パスポートなら2)
-    private int entryCount;// 既に入園した回数
+    private final boolean nightOnly;
+    private int entryCount;
     private final ClockProvider clockProvider;
+
     // ===================================================================================
     //                                                                         Constructor
     //                                                                         ===========
-    public Ticket(int displayPrice, int validDays, boolean nightOnly, ClockProvider clockProvider) { // ← ここに clockProvider を追加
+    /**
+     * チケットを構築する
+     * @param displayPrice チケットに表示される価格
+     * @param validDays チケットの有効日数
+     * @param nightOnly 夜間専用チケットであるか
+     * @param clockProvider 現在時刻を提供するプロバイダ
+     */
+    public Ticket(int displayPrice, int validDays, boolean nightOnly, ClockProvider clockProvider) {
         this.displayPrice = displayPrice;
         this.validDays = validDays;
         this.nightOnly = nightOnly;
@@ -44,37 +54,10 @@ public class Ticket {
     // ===================================================================================
     //                                                                             In Park
     //                                                                             =======
-//    public void doInPark() {
-//        // 有効な入園回数を超えているかチェック
-//        if (entryCount >= validDays) {
-//            // done ayamin [いいね] 例外メッセージ、とってもわかりやすくていいですね。変数情報が入っててデバッグしやすいです by jflute (2025/07/07)
-//            // done ayamin [読み物課題] 例外メッセージ、敬語で満足でもロスロスパターン by jflute (2025/07/07)
-//            // https://jflute.hatenadiary.jp/entry/20170804/explossloss
-//            // (ブログを読むのもjavatry。じっくり読んでくださいませ)
-//            throw new IllegalStateException("値段" + displayPrice + ", validDays=" + validDays + ", entryCount=" + entryCount);
-//        }
-//        // done ayamin [いいね] すごい、ParkContextというシングルトンの概念で制御するようにしているの良いですね by jflute (2025/07/07)
-//        // done ayamin 修行++: せっかくなので、単純なtrue/falseではなく、現在日時で夜かどうか？を判定するようにしてみてください by jflute (2025/07/07)
-//        // ParkContext を書きかえても良いですし、また別の実装にしても良いですし。
-//        // done jflute LocalTime.now(); で現在日時を取得できそうなことがわかったので、これで実装し直してみました by ayamin (2025/07/08)
-//
-//        if (nightOnly && ParkContext.isDay()) { // ParkContext を使用
-//            throw new IllegalStateException("夜ではないので使えません");
-//        }
-//        // 入園回数をインクリメント
-//        entryCount++;
-//    }
-
-    // done ayamin 修行++: LocalTimeを指定できるメソッドをpublicにはしない方良いと思います。 by jflute (2025/07/08)
-    // ちになりますので。
-    // (検索とかだと、呼び出し側に自由に日時を渡せるようにするとかはありますが、今回は業務の振る舞いないので)
-    // (でも、テストの都合上、時間を指定して動作確認したいってのはありますよね...さあそこが課題です)
-    // (このtodoは最後でもOKです)
-    //
-    // 業務のコードでも、現在日時限定の場合は、外から日時を指定できないように作りました。
-    // done ayamin [いいね] おおぉ、ClockProviderインターフェース方式、いいですね！ by jflute (2025/07/24)
-    // TicketBoothから受け取って現在日付を裏から細工できるようになってるの素晴らしいです。
-    // done jflute 1on1にて、ClockProvider一緒に見ていきましょう (2025/07/24)
+    /**
+     * チケットを使って遊園地に入園する
+     * @throws IllegalStateException 入園条件を満たさない場合。
+     */
     public void doInPark() {
         if (entryCount >= validDays) {
             throw new IllegalStateException("有効日数を超えています。現在の入園カウント: " + entryCount + ", 有効日数: " + validDays);
@@ -82,12 +65,7 @@ public class Ticket {
 
         LocalTime currentTime = clockProvider.getCurrentTime();
         if (nightOnly && DayNightChecker.isDay(currentTime)) {
-            // done ayamin [いいね] 例外メッセージ、とても詳しくて素晴らしい by jflute (2025/07/24)
-            // done ayamin 横長すぎるので、現在時刻のところ、変数に抽出しましょう by jflute (2025/07/25)
-            // TODO ayamin 細かいですが、変数名の先頭は小文字で (CurrentTime -> currentTime) by jflute (2025/07/31)
-            // スコープ短いですが、IntelliJのrenameのショートカット機能を使ってrenameしてみましょう。(指トレーニング)
-            // TODO ayamin 細かいですが、java.time.format.DateTimeFormatter のところ、FQCN じゃなくて良いと思います by jflute (2025/07/31)
-            String CurrentTime = currentTime.format(java.time.format.DateTimeFormatter.ofPattern("HH:mm"));
+            String CurrentTime = currentTime.format(DateTimeFormatter.ofPattern("HH:mm"));
             String errorMessage = "このチケットは夜間専用です。現在時刻(" + CurrentTime + ")は昼間のため使用できません。";
             throw new IllegalStateException(errorMessage);
         }
@@ -96,21 +74,45 @@ public class Ticket {
     // ===================================================================================
     //                                                                            Accessor
     //                                                                            ========
+    /**
+     * チケットに表示されている価格を取得
+     * @return チケットの表示価格
+     */
     public int getDisplayPrice() {
         return displayPrice;
     }
+    /**
+     * 一度でも入園したことがあるかを判定
+     * @return 入園済みであればtrue
+     */
     public boolean isAlreadyIn() {
         return entryCount > 0;
     }
+    /**
+     * まだ入園できるかを判定
+     * @return 入園可能であればtrue
+     */
     public boolean canEnterPark() {
         return entryCount < validDays;
     }
+    /**
+     * チケットの有効日数を取得
+     * @return 有効日数
+     */
     public int getValidDays() {
         return validDays;
     }
+    /**
+     * 現在の入園カウントを取得
+     * @return 入園した回数
+     */
     public int getEntryCount() {
         return entryCount;
     }
+    /**
+     * このチケットが夜間専用であるかを判定
+     * @return 夜間専用であればtrue
+     */
     public boolean isNightOnly() {
         return nightOnly;
     }
---
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/Ticket_draft.java
@@ -0,0 +1,119 @@
+///*
+// * Copyright 2019-2022 the original author or authors.
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// * either express or implied. See the License for the specific language
+// * governing permissions and limitations under the License.
+// */
+//package org.docksidestage.bizfw.basic.buyticket;
+//import java.time.LocalTime;
+//import java.time.format.DateTimeFormatter;
+//
+///**
+// * @author jflute
+// * @author ayamin
+// */
+//public class Ticket {
+//
+//    // ===================================================================================
+//    //                                                                           Attribute
+//    //                                                                           =========
+//    // done ayamin [いいね] インスタンス変数の定義順序、Constructorの引数や設定と合っててとても綺麗で直感的で良いです！ by jflute (2025/07/07)
+//    private final int displayPrice; // written on ticket, park guest can watch this
+//    private final int validDays;
+//    private final boolean nightOnly;// チケットが有効な日数 (例: 1日パスポートなら1, 2日パスポートなら2)
+//    private int entryCount;// 既に入園した回数
+//    private final ClockProvider clockProvider;
+//    // ===================================================================================
+//    //                                                                         Constructor
+//    //                                                                         ===========
+//    public Ticket(int displayPrice, int validDays, boolean nightOnly, ClockProvider clockProvider) { // ← ここに clockProvider を追加
+//        this.displayPrice = displayPrice;
+//        this.validDays = validDays;
+//        this.nightOnly = nightOnly;
+//        this.entryCount = 0;
+//        this.clockProvider = clockProvider;
+//    }
+//    // ===================================================================================
+//    //                                                                             In Park
+//    //                                                                             =======
+//    //    public void doInPark() {
+//    //        // 有効な入園回数を超えているかチェック
+//    //        if (entryCount >= validDays) {
+//    //            // done ayamin [いいね] 例外メッセージ、とってもわかりやすくていいですね。変数情報が入っててデバッグしやすいです by jflute (2025/07/07)
+//    //            // done ayamin [読み物課題] 例外メッセージ、敬語で満足でもロスロスパターン by jflute (2025/07/07)
+//    //            // https://jflute.hatenadiary.jp/entry/20170804/explossloss
+//    //            // (ブログを読むのもjavatry。じっくり読んでくださいませ)
+//    //            throw new IllegalStateException("値段" + displayPrice + ", validDays=" + validDays + ", entryCount=" + entryCount);
+//    //        }
+//    //        // done ayamin [いいね] すごい、ParkContextというシングルトンの概念で制御するようにしているの良いですね by jflute (2025/07/07)
+//    //        // done ayamin 修行++: せっかくなので、単純なtrue/falseではなく、現在日時で夜かどうか？を判定するようにしてみてください by jflute (2025/07/07)
+//    //        // ParkContext を書きかえても良いですし、また別の実装にしても良いですし。
+//    //        // done jflute LocalTime.now(); で現在日時を取得できそうなことがわかったので、これで実装し直してみました by ayamin (2025/07/08)
+//    //
+//    //        if (nightOnly && ParkContext.isDay()) { // ParkContext を使用
+//    //            throw new IllegalStateException("夜ではないので使えません");
+//    //        }
+//    //        // 入園回数をインクリメント
+//    //        entryCount++;
+//    //    }
+//
+//    // done ayamin 修行++: LocalTimeを指定できるメソッドをpublicにはしない方良いと思います。 by jflute (2025/07/08)
+//    // ちになりますので。
+//    // (検索とかだと、呼び出し側に自由に日時を渡せるようにするとかはありますが、今回は業務の振る舞いないので)
+//    // (でも、テストの都合上、時間を指定して動作確認したいってのはありますよね...さあそこが課題です)
+//    // (このto.doは最後でもOKです)
+//    //
+//    // 業務のコードでも、現在日時限定の場合は、外から日時を指定できないように作りました。
+//    // done ayamin [いいね] おおぉ、ClockProviderインターフェース方式、いいですね！ by jflute (2025/07/24)
+//    // TicketBoothから受け取って現在日付を裏から細工できるようになってるの素晴らしいです。
+//    // done jflute 1on1にて、ClockProvider一緒に見ていきましょう (2025/07/24)
+//    public void doInPark() {
+//        if (entryCount >= validDays) {
+//            throw new IllegalStateException("有効日数を超えています。現在の入園カウント: " + entryCount + ", 有効日数: " + validDays);
+//        }
+//
+//        LocalTime currentTime = clockProvider.getCurrentTime();
+//        if (nightOnly && DayNightChecker.isDay(currentTime)) {
+//            // done ayamin [いいね] 例外メッセージ、とても詳しくて素晴らしい by jflute (2025/07/24)
+//            // done ayamin 横長すぎるので、現在時刻のところ、変数に抽出しましょう by jflute (2025/07/25)
+//            // TODO done ayamin 細かいですが、変数名の先頭は小文字で (CurrentTime -> currentTime) by jflute (2025/07/31)
+//            // スコープ短いですが、IntelliJのrenameのショートカット機能を使ってrenameしてみましょう。(指トレーニング)
+//            // TODO done ayamin 細かいですが、java.time.format.DateTimeFormatter のところ、FQCN じゃなくて良いと思います by jflute (2025/07/31)
+//            String CurrentTime = currentTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+//            String errorMessage = "このチケットは夜間専用です。現在時刻(" + CurrentTime + ")は昼間のため使用できません。";
+//            throw new IllegalStateException(errorMessage);
+//        }
+//        entryCount++;
+//    }
+//    // ===================================================================================
+//    //                                                                            Accessor
+//    //                                                                            ========
+//    public int getDisplayPrice() {
+//        return displayPrice;
+//    }
+//    public boolean isAlreadyIn() {
+//        return entryCount > 0;
+//    }
+//    public boolean canEnterPark() {
+//        return entryCount < validDays;
+//    }
+//    public int getValidDays() {
+//        return validDays;
+//    }
+//    public int getEntryCount() {
+//        return entryCount;
+//    }
+//    public boolean isNightOnly() {
+//        return nightOnly;
+//    }
+//}
+//
